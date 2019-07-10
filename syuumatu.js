8412cdbe34b6830eefa871ee39e8dc26d8f33f51
Magubanua Ajin Sunga <ajin@MagubanuanoMacBook-puro.local>
2019-07-10 09:17:36 +0800

    hello

---
+++ syuumatu.js
@@ -0,0 +1,7 @@
+// Regular map
+function regular_map() {
+    var var_location = new google.maps.LatLng(40.725118, -73.997699);
+
+    var var_mapoptions = {
+        center: var_location,
+            zoom: }
